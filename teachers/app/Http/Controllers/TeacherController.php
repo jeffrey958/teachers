c9b48522f9b555395a66413f38e3956082b88150
F308USER <jeffrey862286@gmail.com>
2017-12-15 14:19:57 +0800

    no message

---
+++ teachers/app/Http/Controllers/TeacherController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    //
+	public function index(){
+		return '顯示所有老師';
+	}
+
+
+
+}
---
+++ teachers/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return '彭胤瑜 D1044182061';
+});
+
+Route::get('/teachers','TeachersController@index');
+
+Route::get('/teachers/{id}',function($id){
+
+},'TeachersController@');
